21307036aff89acd2b22ca7386d538cab76b84f5
Mathéo Vignaud <64284499+MatheoVignaud@users.noreply.github.com>
2026-03-09 23:37:37 +0100

    Refactor xmake.lua to streamline OpenMP configuration and remove unnecessary dependencies

--- xmake.lua
+++ xmake.lua
@@ -3,19 +3,16 @@ add_rules("mode.debug", "mode.release")
 set_languages("c17")
 set_defaultmode("release")
 
-add_requires("openmp")
-
-
 target("VirtuaPPU")
     set_kind("static")
-    add_deps("openmp")
+    if is_plat("windows") then
+        set_plat("mingw")
+        set_toolchains("mingw")
+    end
     add_includedirs("include", {public = true})
     add_headerfiles("include/**.h")
     add_files("src/*.c")
     add_defines("USE_OPENMP")
-    if is_plat("windows") then
-        add_cflags("/openmp", {tools = {"cl", "clang_cl"}})
-    else
-        add_cflags("-fopenmp", {tools = {"gcc", "clang"}})
-        add_ldflags("-fopenmp", {tools = {"gcc", "clang"}})
-    end
+    add_cflags("-fopenmp", {tools = {"gcc", "clang"}})
+    add_ldflags("-fopenmp", {tools = {"gcc", "clang"}})
+    add_syslinks("gomp", {public = true})
